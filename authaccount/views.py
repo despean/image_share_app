bc3dddfe893ee5df4c3d6176a84667706891ab59
despean <despean15@gmail.com>
2017-06-07 22:15:07 +0100

    deploy

--- authaccount/views.py
+++ authaccount/views.py
@@ -78,7 +78,7 @@ def edit(request):
     if user_form.is_valid() and profile_form.is_valid():
       user_form.save()
       profile_form.save()
-      messages.success(request, 'Procfile updated ' \
+      messages.success(request, 'Profile updated ' \
                                 'successfully')
       return redirect('/account')
     else:

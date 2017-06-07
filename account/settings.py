a5146dd0d38c5a6518412a6e1090c7b645933c97
despean <despean15@gmail.com>
2017-06-07 22:45:06 +0100

    deploy

--- account/settings.py
+++ account/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cqwfsl!+q55li=wt36b28bh292n&(u1c=#()f(x47k19sld=(m'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-  'imageshareapp.herokuapp.com'
+  ' imagebookmarkshare.herokuapp.com'
 ]
 
 # Application definition

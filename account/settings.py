a8c449eb81f82e4f9f3f5352f6fb14d147328c0e
despean <despean15@gmail.com>
2017-06-07 23:06:10 +0100

    Update settings.py

--- account/settings.py
+++ account/settings.py
@@ -193,8 +193,8 @@ LOGOUT_REDIRECT_URL = reverse_lazy('account:login')
 # email backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djangoappmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'stdjango'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
